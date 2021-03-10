d471a610728176b5862752388fadd60e3d3f0452
Sepideh Miralaei <sepidehmiralaei@Sepidehs-MacBook-Pro.local>
2021-03-10 16:43:37 -0500

    added prod url for heroku

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
-export const API_ROOT = 'http://localhost:4000';
+const DEV_URL = 'http://localhost:4000/';
+const PROD_URL = 'https://sepid-chatapi.herokuapp.com/';
+export const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
 export const API_WS_ROOT = 'ws://localhost:4000/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
